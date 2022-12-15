eaaea817e298b7fc6f7c6a171c42b85a603467af
rcarabal2022 <raquelcaraballospain@gmail.com>
2022-12-15 20:24:58 +0100

    Agregar fondo a la pagina web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import "./App.css";
+import Logo from './assets/LogoTarot.png';
 
 function App() {
   return (
     <>
-
+      <header>
+        <img  src={Logo}className="LogoTarot" alt="logoTarotSakura" />
+      </header>
     </>
   );
 }
 
-export default App;
+export default App; 
