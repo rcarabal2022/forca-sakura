fa1159161316ccc92c7d336cb9dad774801629a4
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2022-12-20 12:36:12 +0100

    fetch-api titulo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Router from './aplication/Router';
+import Router from './applications/Router.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
